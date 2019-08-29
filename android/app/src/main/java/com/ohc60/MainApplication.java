78360a38552aeb7e828350c4cca0f78c857b675d
Ravi Kumar <ravi.kumar@smartdatainc.net>
2019-08-29 10:13:58 +0530

    Initial Commit

---
+++ android/app/src/main/java/com/ohc60/MainApplication.java
@@ -0,0 +1,58 @@
+package com.ohc60;
+
+import android.app.Application;
+import android.util.Log;
+import java.util.Arrays;
+
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
+import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.List;
+
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
+
+public class MainApplication extends NavigationApplication {
+  
+      @Override
+      protected ReactGateway createReactGateway() {
+          ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+              @Override
+              protected String getJSMainModuleName() {
+                  return "index";
+              }
+          };
+          return new ReactGateway(this, isDebug(), host);
+      }
+  
+      @Override
+      public boolean isDebug() {
+          return BuildConfig.DEBUG;
+      }
+  
+      protected List<ReactPackage> getPackages() {
+          // Add additional packages you require here
+          // No need to add RnnPackage and MainReactPackage
+          return Arrays.<ReactPackage>asList(
+              // eg. new VectorIconsPackage()
+              new SplashScreenReactPackage() 
+          );
+      }
+
+
+
+    
+  
+      @Override
+      public List<ReactPackage> createAdditionalReactPackages() {
+          return getPackages();
+      }
+}
---
+++ ios/Pods/Target Support Files/Pods-OHC60/Pods-OHC60-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OHC60 : NSObject
+@end
+@implementation PodsDummy_Pods_OHC60
+@end
